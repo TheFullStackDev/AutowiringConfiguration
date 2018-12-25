5ab4778aefdbe1678f14b6984b621fd35cbc221b
dibasbehera7 <dibasbehera@gmail.com>
2018-12-25 20:53:34 +0530

    Autowired using Value annotation

--- AutowiringConfiguration/src/main/java/com/dibas/MyApp.java
+++ AutowiringConfiguration/src/main/java/com/dibas/MyApp.java
@@ -3,6 +3,7 @@ package com.dibas;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
+import com.dibas.food.Fruit;
 import com.dibas.food.Meal;
 
 public class MyApp {
@@ -10,11 +11,15 @@ public class MyApp {
 	public static void main(String[] args) {
 
 		ApplicationContext appContext = new FileSystemXmlApplicationContext("appContext.xml");
-		
-		
-		Meal myMealByAnnotation = (Meal) appContext.getBean("mealByAnnotation");
-		System.out.println("MealByAnnotation : "+myMealByAnnotation.whatsInTodaysMeal());
-			
+
+		/*
+		 * Meal myMealByAnnotation = (Meal) appContext.getBean("mealByAnnotation");
+		 * System.out.println("MealByAnnotation : "+myMealByAnnotation.whatsInTodaysMeal
+		 * ());
+		 */
+
+		Fruit myfruit = (Fruit) appContext.getBean("fruit");
+		System.out.println(myfruit.talkAboutYou());
 		
 		((FileSystemXmlApplicationContext) appContext).close();
 
--- AutowiringConfiguration/src/main/java/com/dibas/food/Fruit.java
+++ AutowiringConfiguration/src/main/java/com/dibas/food/Fruit.java
@@ -1,6 +1,21 @@
 package com.dibas.food;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
 public class Fruit {
+
+	
+	private String description = "not set";
+	
+	public String getDescription() {
+		return description;
+	}
+
+	@Autowired
+	public void setDescription(@Value("Hi, I'm Fruit.") String description) {
+		this.description = description;
+	}
 
 	public Fruit() {
 		// no arg constructor
@@ -8,7 +23,7 @@ public class Fruit {
 
 	public String talkAboutYou() {
 
-		return "Hi, I'm Fruit. ";
+		return description;
 
 	}
 		
