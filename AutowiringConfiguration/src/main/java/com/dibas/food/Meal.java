2ed1c42b19579889631fa8fd7b77b8ddd9ad6975
dibasbehera7 <dibasbehera@gmail.com>
2018-12-25 15:58:25 +0530

    Autowiring Properties

--- AutowiringConfiguration/src/main/java/com/dibas/food/Meal.java
+++ AutowiringConfiguration/src/main/java/com/dibas/food/Meal.java
@@ -1,58 +1,76 @@
-	package com.dibas.food;
+package com.dibas.food;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class Meal {
 
+	@Autowired
 	private Fruit fruit;
+	@Autowired
 	private Dairy dairy;
+	@Autowired
 	private Grain grain;
+	@Autowired
 	private Meat meat;
-	private Vegetable veg;
-	 
-		
 	
+	private Vegetable veg;
+
 	public Dairy getDairy() {
 		return dairy;
 	}
+
 	public void setDairy(Dairy dairy) {
 		this.dairy = dairy;
 	}
+
 	public Fruit getFruit() {
 		return fruit;
 	}
+
 	public void setFruit(Fruit fruit) {
 		this.fruit = fruit;
 	}
+
 	public Grain getGrain() {
 		return grain;
 	}
+
 	public void setGrain(Grain grain) {
 		this.grain = grain;
 	}
+
 	public Meat getMeat() {
 		return meat;
 	}
+
 	public void setMeat(Meat meat) {
 		this.meat = meat;
 	}
+
 	public Vegetable getveg() {
 		return veg;
 	}
+	@Autowired
 	public void setveg(Vegetable veg) {
 		this.veg = veg;
 	}
-	
+
 	public String whatsInTodaysMeal() {
-		
+
 		String menu = "Today's Meal contains:\n";
-		
-		if(fruit!=null) menu += "some fruits: ";
-		if(dairy!=null) menu += "some dairy: ";
-		if(grain!=null) menu += "some grain: ";
-		if(meat!=null) menu += "some meat: ";
-		if(veg!=null) menu +="some vegs: ";
- 		
+
+		if (fruit != null)
+			menu += "some fruits: ";
+		if (dairy != null)
+			menu += "some dairy: ";
+		if (grain != null)
+			menu += "some grain: ";
+		if (meat != null)
+			menu += "some meat: ";
+		if (veg != null)
+			menu += "some vegs: ";
+
 		return menu;
 	}
-	
-	
+
 }
